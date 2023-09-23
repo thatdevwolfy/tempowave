7cb2e57e13be14156f6a20e77f89b2a70e345aea
TheLordW0lfy <135386517+TheLordW0lfy@users.noreply.github.com>
2023-09-24 00:23:06 +0100

    Update useOnPlay.ts

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -16,10 +16,6 @@ const useOnPlay = (songs: Song[]) => {
       return authModal.onOpen();
     }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
-
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
   }
