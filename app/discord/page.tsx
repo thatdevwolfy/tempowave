43a7035c5b36dd8df9e1fc8fb2565471865deed7
TheLordW0lfy <135386517+TheLordW0lfy@users.noreply.github.com>
2023-09-24 00:49:13 +0100

    Update page.tsx

--- app/discord/page.tsx
+++ app/discord/page.tsx
@@ -1,27 +1,11 @@
-"use client";
-// Import useState from "react/client" instead of "react"
 import { useEffect } from "react";
-import { useState } from "react/client";
 
 const DiscordPage = () => {
-  const [dotVisible, setDotVisible] = useState(true);
-
   useEffect(() => {
-    // Redirect to your Discord invite link after a delay
-    const redirectTimer = setTimeout(() => {
+    // Redirect to your Discord invite link after a delay (replace with your link)
+    setTimeout(() => {
       window.location.href = "YOUR_DISCORD_INVITE_LINK_HERE";
-    }, 5000); // 5000ms (5 seconds) delay
-
-    // Toggle the visibility of the dot every 500ms (0.5 seconds)
-    const dotTimer = setInterval(() => {
-      setDotVisible((prev) => !prev);
-    }, 500);
-
-    // Cleanup timers on unmount
-    return () => {
-      clearTimeout(redirectTimer);
-      clearInterval(dotTimer);
-    };
+    }, 3000); // 3000ms (3 seconds) delay before redirect
   }, []);
 
   return (
@@ -37,28 +21,6 @@ const DiscordPage = () => {
       }}
     >
       <h1 style={{ fontSize: "2rem" }}>Redirecting...</h1>
-      <div style={{ marginTop: "20px" }}>
-        {dotVisible && <span style={{ fontSize: "2rem" }}>.</span>}
-      </div>
-      <div
-        style={{
-          width: "100%",
-          height: "2px",
-          background: "white",
-          position: "relative",
-          marginTop: "20px",
-        }}
-      >
-        <div
-          style={{
-            width: "0%",
-            height: "100%",
-            background: "green",
-            position: "absolute",
-            transition: "width 4s ease-in-out",
-          }}
-        ></div>
-      </div>
     </div>
   );
 };
