0e63c94c3859cefd92c44f7c0da5a4a2ea5b2498
TheLordW0lfy <135386517+TheLordW0lfy@users.noreply.github.com>
2023-09-24 02:19:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ import './globals.css'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
+  title: 'SpoLify',
+  description: 'Media Library',
 }
 
 export const revalidate = 0;
